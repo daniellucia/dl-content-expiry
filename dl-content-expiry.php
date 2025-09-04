299dd82c79885214bc996d9548c6f36887227139
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-04 10:02:15 +0200

    Subimos de versión

--- dl-content-expiry.php
+++ dl-content-expiry.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Content expiry
  * Description: Expira el contenido después de un tiempo determinado.
- * Version: 0.0.3
+ * Version: 0.0.4
  * Author: Daniel Lúcia
  * Author URI: http://www.daniellucia.es
  * textdomain: dl-content-expiry
@@ -13,7 +13,7 @@ defined('ABSPATH') || exit;
 
 require_once __DIR__ . '/src/Plugin.php';
 
-define('DL_CONTENT_EXPIRY_VERSION', '0.0.3');
+define('DL_CONTENT_EXPIRY_VERSION', '0.0.4');
 define('DL_CONTENT_EXPIRY_FILE', __FILE__);
 
 add_action('plugins_loaded', function () {
