8554a01af342093a519f30014a4b643d57d67bbe
Daniel Lúcia <daniellucia84@gmail.com>
2025-09-19 07:32:34 +0200

    Correccion para añadirlo en el hook correcto

--- dl-content-expiry.php
+++ dl-content-expiry.php
@@ -37,7 +37,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 define('DL_CONTENT_EXPIRY_VERSION', '0.0.4');
 define('DL_CONTENT_EXPIRY_FILE', __FILE__);
 
-add_action('plugins_loaded', function () {
+add_action('init', function () {
 
     load_plugin_textdomain('dl-content-expiry', false, dirname(plugin_basename(__FILE__)) . '/languages');
 
